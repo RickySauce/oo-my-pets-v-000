5601d9c3d3a78602e82e78fe4871390893195dbd
Eric DeSimone <eric.desimone@cortland.edu>
2018-05-07 11:28:45 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -75,8 +75,7 @@ class Owner
   def sell_pets
     @pets.each do |pets, animals|
        animals.each do |animal|
-         animal.mood = "nervous"
-         animal.delete
+         animal.mood = "nervous" animal.clear
        end
      end
   end
